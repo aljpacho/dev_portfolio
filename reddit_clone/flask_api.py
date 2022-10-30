7e31a96203655912094add6aba85292f402ea5e8
Jordan Pacho <trainee.jordan.pacho@sigmalabs.co.uk>
2022-10-30 09:40:22 +0000

    Added - db class model for story

--- reddit_clone/flask_api.py
+++ reddit_clone/flask_api.py
@@ -1,7 +1,28 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+import os
+from dotenv import load_dotenv
+from datetime import datetime
+
+load_dotenv()
 
 app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("SQLALCHEMY_DATABASE_URI")
+
+db = SQLAlchemy()
+
+# Setting up db model 
+class story(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String, nullable=False)
+    url = db.Columnn(db.String, nullable=False)
+    created_at = db.Column(db.Datetime, nullable=False, default=datetime.now())
+    updated_at = db.Column(db.Datetime, nullable=False, default=datetime.now())
+
+    def __repr__(self):
+        return self.title
+
 
 
 @app.route("/")
@@ -20,4 +41,4 @@ def get_story_by_story_id():
 
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
