8e5019a7956fb05a903a674dd10ea4184c9533be
Jordan Pacho <trainee.jordan.pacho@sigmalabs.co.uk>
2022-11-11 08:16:30 +0000

    Added - commit to class method

--- reddit_clone/webscraper/webscraper_to_db.py
+++ reddit_clone/webscraper/webscraper_to_db.py
@@ -1,5 +1,6 @@
-import psycopg2
 import os
+
+import psycopg2
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -7,28 +8,39 @@ load_dotenv()
 LOCAL_DB_NAME = os.getenv("LOCAL_DB_NAME")
 LOCAL_DB_USER = os.getenv("LOCAL_DB_USER")
 
-# TODO: 
-# Methods to implement
-# connect_to_db 
-# get_stories
-# insert_stories
-
 class DBConnection:
     def __init__(self, db_name=LOCAL_DB_NAME, db_user=LOCAL_DB_USER):
         self._name = db_name
-        self.conn = psycopg2.connect(host="localhost", database=LOCAL_DB_NAME, user=db_user)
+        self.conn = psycopg2.connect(host="localhost", database=db_name, user=db_user)
 
     def add_stories(self, stories):
+        try:
+            cursor = self.conn.cursor()
+            cursor.executemany(
+                """
+                INSERT INTO stories (title, url, created_at)
+                VALUES (%(title)s, %(url)s, %(created_at)s);
+            """,
+                stories,
+            )
+
+            print("Data inserted into database")
+        except psycopg2.DatabaseError as e:
+            return e
+        else:
+            self.conn.commit()
+        finally:
+            cursor.close()
+            self.conn.close()
+
+    def get_stories(self):
         try:
             cursor = self.conn.cursor()
         except psycopg2.DatabaseError as e:
             return e
         else:
-            for story in stories:
-                cursor.execute("""
-                    INSERT INTO stories (title, author, url, created_at)
-                    VALUES ({title}, {author}, {url}, {created_at})
-                """.format(**story))
+            cursor.execute("""SELECT * FROM stories;""")
+            print(cursor.fetchall())
         finally:
             cursor.close()
-
+            self.conn.close()
