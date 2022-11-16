08940de0024912d4e95dceb15bee4419ba9ee843
Jordan Pacho <trainee.jordan.pacho@sigmalabs.co.uk>
2022-11-16 15:37:07 +0000

    Added - function to update score

--- number-guesser-starting/script.js
+++ number-guesser-starting/script.js
@@ -10,18 +10,32 @@ function generateTarget() {
   return Math.floor(Math.random() * 9);
 }
 
-/** 
-* Compares player and computer guesses to see what was closer to the target
-* @param {Number} playerGuess - a number between 0-9
-* @param {Number} computerGuess - a number between 0-9
-* @param {Number} target - a random number between 0-9
-* @return {Boolean} true means the player has won and false the computer wins
-*/
-function compareGuesses(playerGuess, computerGuess, target) {
-  const playerDiff = Math.abs(playerGuess - target);
+/**
+ * Compares player and computer guesses to see what was closer to the target
+ * @param {Number} humanGuess - a number between 0-9
+ * @param {Number} computerGuess - a number between 0-9
+ * @param {Number} target - a random number between 0-9
+ * @return {Boolean} true means the player has won and false the computer wins
+ */
+function compareGuesses(humanGuess, computerGuess, target) {
+  const playerDiff = Math.abs(humanGuess - target);
   const computerDiff = Math.abs(computerGuess - target);
   return playerDiff <= computerDiff ? true : false;
 }
+
+/** 
+* Increments either the humanScore or computer score depending on the input
+* @param {String} winner - either 'human' or 'computer'
+*/
+function updateScore(winner) {
+  if (winner === "human") {
+    humanScore++;
+  } else {
+    computerScore++;
+  }
+}
+
+
 
 function main() {
   const target = generateTarget();
