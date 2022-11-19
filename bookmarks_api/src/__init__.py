d872bba76b5f71a4c5ad3a2aae18598e42aedc10
Jordan Pacho <trainee.jordan.pacho@sigmalabs.co.uk>
2022-11-19 22:16:25 +0000

    Changed dir

--- bookmarks_api/src/__init__.py
+++ bookmarks_api/src/__init__.py
@@ -2,6 +2,9 @@ import os
 
 from flask import Flask
 
+from src.auth import auth
+from src.bookmarks import bookmarks
+
 
 def create_app(test_config=None):
 
@@ -12,8 +15,7 @@ def create_app(test_config=None):
     else:
         app.config.from_mapping(test_config)
 
-    @app.get("/")
-    def index():
-        return "test"
+    app.register_blueprint(auth)
+    app.register_blueprint(bookmarks)
 
     return app
