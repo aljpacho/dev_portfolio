ac3d2667b8becc09128c6cdd4a0473c67811b431
Jordan Pacho <trainee.jordan.pacho@sigmalabs.co.uk>
2022-11-20 16:35:54 +0000

    Fixed typo Datetime to DateTime

--- bookmarks_api/src/database.py
+++ bookmarks_api/src/database.py
@@ -8,12 +8,12 @@ db = SQLAlchemy()
 
 
 class User(db.Model):
-    id = db.column(db.Integer, primary_key=True)
-    username = db.column(db.String(80), unique=True, nullable=False)
-    email = db.column(db.String(120), unique=True, nullable=False)
-    password = db.column(db.Text, nullable=False)
-    created_at = db.column(db.DateTime, default=datetime.now())
-    updated_at = db.column(db.Datetime, onupdate=datetime.now())
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password = db.Column(db.Text, nullable=False)
+    created_at = db.Column(db.DateTime, default=datetime.now())
+    updated_at = db.Column(db.DateTime, onupdate=datetime.now())
     bookmarks = db.relationship("Bookmark", backref="user")
 
     def __repr__(self):
@@ -21,14 +21,14 @@ class User(db.Model):
 
 
 class Bookmark(db.Model):
-    id = db.column(db.Integer, primary_key=True)
-    body = db.column(db.Text, nullable=True)
-    url = db.column(db.Text, nullable=False)
-    short_url = db.column(db.String(3), nullable=True)
-    visits = db.column(db.Integer, default=0)
-    created_at = db.column(db.DateTime, default=datetime.now())
-    updated_at = db.column(db.Datetime, onupdate=datetime.now())
-    user_id = db.column(db.Integer, db.ForeignKey("user.id"))
+    id = db.Column(db.Integer, primary_key=True)
+    body = db.Column(db.Text, nullable=True)
+    url = db.Column(db.Text, nullable=False)
+    short_url = db.Column(db.String(3), nullable=True)
+    visits = db.Column(db.Integer, default=0)
+    created_at = db.Column(db.DateTime, default=datetime.now())
+    updated_at = db.Column(db.DateTime, onupdate=datetime.now())
+    user_id = db.Column(db.Integer, db.ForeignKey("user.id"))
 
     def generate_short_id(self, num_of_chars=3) -> str:
         """Generates a short ID using all digits and ASCII letters
