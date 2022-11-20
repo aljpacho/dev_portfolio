245dcb5acae1dffec247ab09025d49cbe7f66f52
Jordan Pacho <trainee.jordan.pacho@sigmalabs.co.uk>
2022-11-20 17:03:20 +0000

    Added password length verification

--- bookmarks_api/src/auth.py
+++ bookmarks_api/src/auth.py
@@ -1,7 +1,23 @@
-from flask import Blueprint
+from http import HTTPStatus
 
-auth = Blueprint("auth",__name__, url_prefix="/api/v1/auth")
+from flask import Blueprint, jsonify, request
+from werkzeug.security import check_password_hash, generate_password_hash
 
-@auth.post('/register')
+auth = Blueprint("auth", __name__, url_prefix="/api/v1/auth")
+
+
+@auth.post("/register")
 def register():
+    username = request.json["username"]
+    email = request.json["email"]
+    password = request.json["password"]
+
+    if len(password) < 8:
+        return (
+            jsonify({"error": "Password is too short"}),
+            HTTPStatus.BAD_REQUEST.value,
+            HTTPStatus.BAD_REQUEST.phrase,
+        )
+    
+
     return "User created"
