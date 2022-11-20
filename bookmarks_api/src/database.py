d8422440aed07081ac6efcd4a20f542f0dd9dda8
Jordan Pacho <trainee.jordan.pacho@sigmalabs.co.uk>
2022-11-20 14:35:24 +0000

    Added User class to database

---
+++ bookmarks_api/src/database.py
@@ -0,0 +1,18 @@
+from datetime import datetime
+
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+
+class User(db.Model):
+    id = db.column(db.Integer, primary_key=True)
+    username = db.column(db.String(80), unique=True, nullable=False)
+    email = db.column(db.String(120), unique=True, nullable=False)
+    password = db.column(db.Text(), nullable=False)
+    created_at = db.column(db.DateTime, default=datetime.now())
+    updated_at = db.column(db.Datetime, onupdate=datetime.now())
+
+
+    def __repr__(self):
+        return f'User: {self.username}'
