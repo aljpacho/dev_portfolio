469bc4093beb8f17014dc49dd50da2a6f88bf983
Jordan Pacho <trainee.jordan.pacho@sigmalabs.co.uk>
2022-11-20 16:35:34 +0000

    Added database connection to postgres

--- bookmarks_api/src/__init__.py
+++ bookmarks_api/src/__init__.py
@@ -4,6 +4,7 @@ from flask import Flask
 
 from src.auth import auth
 from src.bookmarks import bookmarks
+from src.database import db
 
 
 def create_app(test_config=None):
@@ -11,10 +12,15 @@ def create_app(test_config=None):
     app = Flask(__name__, instance_relative_config=True)
 
     if test_config is None:
-        app.config.from_mapping(SECRET_KEY=os.environ.get("BOOKMARKS_DEV_SK"))
+        app.config.from_mapping(
+            SECRET_KEY=os.environ.get("BOOKMARKS_DEV_SK"),
+            SQLALCHEMY_DATABASE_URI=os.environ.get("BOOKMARKS_DB_URI"),
+        )
     else:
         app.config.from_mapping(test_config)
 
+    db.app = app
+    db.init_app(app)
     app.register_blueprint(auth)
     app.register_blueprint(bookmarks)
 
