0386619085750bc24fcb9c22c39c8e513088e9bd
Jordan Pacho <trainee.jordan.pacho@sigmalabs.co.uk>
2022-11-23 13:51:33 +0000

    Added JWTManger for token auth

--- bookmarks_api/src/__init__.py
+++ bookmarks_api/src/__init__.py
@@ -5,6 +5,7 @@ from flask import Flask
 from src.auth import auth
 from src.bookmarks import bookmarks
 from src.database import db
+from flask_jwt_extended import JWTManager
 
 
 def create_app(test_config=None):
@@ -15,12 +16,16 @@ def create_app(test_config=None):
         app.config.from_mapping(
             SECRET_KEY=os.environ.get("BOOKMARKS_DEV_SK"),
             SQLALCHEMY_DATABASE_URI=os.environ.get("BOOKMARKS_DB_URI"),
+            JWT_SECRET_KEY=os.environ.get("JWT_SECRET_KEY")
         )
     else:
         app.config.from_mapping(test_config)
 
     db.app = app
     db.init_app(app)
+
+    JWTManager(app)
+
     app.register_blueprint(auth)
     app.register_blueprint(bookmarks)
 
