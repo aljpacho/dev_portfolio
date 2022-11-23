a89777d309744f5cb280df377733a5407344aac7
Jordan Pacho <trainee.jordan.pacho@sigmalabs.co.uk>
2022-11-23 12:20:51 +0000

    Added user, email auth and pwd gen

--- bookmarks_api/src/auth.py
+++ bookmarks_api/src/auth.py
@@ -1,7 +1,10 @@
 from http import HTTPStatus
 
+import validators
 from flask import Blueprint, jsonify, request
 from werkzeug.security import check_password_hash, generate_password_hash
+
+from src.database import User, db
 
 auth = Blueprint("auth", __name__, url_prefix="/api/v1/auth")
 
@@ -12,24 +15,60 @@ def register():
     email = request.json["email"]
     password = request.json["password"]
 
-    if username.isalnum():
+    if len(username) < 4:
         return (
-            jsonify({"error": "Username is not alphanumeric or contains whitespace"}),
-            HTTPStatus.BAD_REQUEST.value,
-            HTTPStatus.BAD_REQUEST.phrase,
-        )
-    elif len(username) < 4:
-        return (
-            jsonify({"error": "Username is too short. Must be at least 5 characters."}),
+            jsonify({"Error": "Username is too short. Must be at least 5 characters."}),
             HTTPStatus.BAD_REQUEST.value,
             HTTPStatus.BAD_REQUEST.phrase,
         )
 
     if len(password) < 8:
         return (
-            jsonify({"error": "Password is too short"}),
+            jsonify({"Error": "Password is too short"}),
             HTTPStatus.BAD_REQUEST.value,
             HTTPStatus.BAD_REQUEST.phrase,
         )
 
-    return "User created"
+    if not username.isalnum():
+        return (
+            jsonify({"Error": "Username is not alphanumeric or contains whitespace"}),
+            HTTPStatus.BAD_REQUEST.value,
+            HTTPStatus.BAD_REQUEST.phrase,
+        )
+
+    if User.objects.filter_by(email=username).first() is not None:
+        return (
+            {"Error": "Username is taken"},
+            HTTPStatus.CONFLICT.value,
+            HTTPStatus.CONFLICT.phrase,
+        )
+
+    if not validators.email(email):
+        return (
+            jsonify({"Error": "Email is not valid"}),
+            HTTPStatus.BAD_REQUEST.value,
+            HTTPStatus.BAD_REQUEST.phrase,
+        )
+
+    if User.objects.filter_by(email=email).first() is not None:
+        return (
+            {"Error": "Email is taken"},
+            HTTPStatus.CONFLICT.value,
+            HTTPStatus.CONFLICT.phrase,
+        )
+
+    password_hash = generate_password_hash(password)
+
+    new_user = User(username=username, password=password_hash, email=email)
+
+    db.session.add(new_user)
+
+    db.session.commit()
+
+    return (
+        jsonify(
+            {"Message": "User Created", "User": {"Username": username, "Email": email}}
+        ),
+        HTTPStatus.CREATED.value,
+        HTTPStatus.CREATED.phrase,
+    )
