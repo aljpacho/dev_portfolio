6ad8816859e5cbb25ff0bcd4530b7e38faad8ca4
Jordan Pacho <trainee.jordan.pacho@sigmalabs.co.uk>
2022-11-24 13:24:15 +0000

    Added redirect for short_url

--- bookmarks_api/src/__init__.py
+++ bookmarks_api/src/__init__.py
@@ -1,11 +1,11 @@
 import os
 
-from flask import Flask
+from flask import Flask, redirect
+from flask_jwt_extended import JWTManager
 
 from src.auth import auth
 from src.bookmarks import bookmarks
-from src.database import db
-from flask_jwt_extended import JWTManager
+from src.database import Bookmark, db
 
 
 def create_app(test_config=None):
@@ -16,7 +16,7 @@ def create_app(test_config=None):
         app.config.from_mapping(
             SECRET_KEY=os.environ.get("BOOKMARKS_DEV_SK"),
             SQLALCHEMY_DATABASE_URI=os.environ.get("BOOKMARKS_DB_URI"),
-            JWT_SECRET_KEY=os.environ.get("JWT_SECRET_KEY")
+            JWT_SECRET_KEY=os.environ.get("JWT_SECRET_KEY"),
         )
     else:
         app.config.from_mapping(test_config)
@@ -28,5 +28,14 @@ def create_app(test_config=None):
 
     app.register_blueprint(auth)
     app.register_blueprint(bookmarks)
+
+    @app.get("/<short_url>")
+    def redirect_to_url(short_url):
+        bookmark_query = Bookmark.query.filter_by(short_url=short_url).first_or_404()
+
+        if bookmark_query:
+            bookmark_query.visits = bookmark_query.visits + 1
+            db.session.commit()
+            return redirect(bookmark_query.url)
 
     return app
