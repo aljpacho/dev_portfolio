d5407e004b90a764af7e298d847914ef5cdd5e71
Jordan Pacho <trainee.jordan.pacho@sigmalabs.co.uk>
2022-12-07 22:01:24 +0000

    Added new unit test class for hand

--- blackjack-oop/unit-tests-blackjack.py
+++ blackjack-oop/unit-tests-blackjack.py
@@ -1,15 +1,21 @@
 import unittest
 
-import blackjack
+from blackjack import *
 
 
 class TestDeck(unittest.TestCase):
     def setUp(self):
-        self.deck = blackjack.Deck()
+        self.deck = Deck()
+
+    def test_str_card(self):
+        """Check __str__ method for a card"""
+        card = Card("1", "Diamonds")
+        self.assertEquals(str(card), "1-Diamonds")
 
     def test_deck_generation(self):
         """Check to see if deck generated in the correct order of rank-suit"""
         self.assertEqual(
+            self.deck.get_cards(),
             [
                 "2-Hearts",
                 "2-Clubs",
@@ -64,8 +70,20 @@ class TestDeck(unittest.TestCase):
                 "A-Spades",
                 "A-Diamonds",
             ],
-            self.deck.get_cards(),
         )
+
+
+class TestHand(unittest.TestCase):
+    def setUp(self):
+        self.deck = Deck()
+
+    def test_hand_value(self):
+        """Check to see if cards add to correct value when in the hand"""
+        card_1 = Card("2", "Diamonds")
+        card_2 = Card("5", "Hearts")
+        player_hand = Hand([card_1, card_2])
+
+        self.assertEquals(player_hand.get_value(), 7)
 
 
 if __name__ == "__main__":
